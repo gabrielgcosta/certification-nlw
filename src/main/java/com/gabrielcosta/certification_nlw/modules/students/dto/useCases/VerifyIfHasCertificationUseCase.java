1d0b533e0cd8a3c1436f8733f40fb20cf64920b3
gabrielgcosta <gabriel.costa@codenapp.com>
2024-02-07 21:14:59 -0300

    creating CertificationStudentRepository

--- src/main/java/com/gabrielcosta/certification_nlw/modules/students/dto/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/gabrielcosta/certification_nlw/modules/students/dto/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,14 +1,24 @@
 package com.gabrielcosta.certification_nlw.modules.students.dto.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabrielcosta.certification_nlw.modules.students.dto.VerifyIfHasCertificationDTO;
+import com.gabrielcosta.certification_nlw.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
+    @Autowired
+    CertificationStudentRepository certificationStudentRepository;
+
     public boolean execute(VerifyIfHasCertificationDTO dto){
-        return true;
+        var result = certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+
+        if(!result.isEmpty()){
+            return true;
+        }
+        return false;
     }
     
 }
