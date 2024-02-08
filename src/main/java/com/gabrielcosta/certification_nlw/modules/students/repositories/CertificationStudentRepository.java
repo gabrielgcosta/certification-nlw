96c77f223b985fcfdd5a18fffd4d3639468c3149
gabrielgcosta <gabriel.costa@codenapp.com>
2024-02-08 10:10:03 -0300

    creating /questions/technology/{technology} route

--- src/main/java/com/gabrielcosta/certification_nlw/modules/students/repositories/CertificationStudentRepository.java
+++ src/main/java/com/gabrielcosta/certification_nlw/modules/students/repositories/CertificationStudentRepository.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.gabrielcosta.certification_nlw.modules.students.entities.CertificationStudentEntity;
 
+@Repository
 public interface CertificationStudentRepository extends JpaRepository<CertificationStudentEntity, UUID> {
 
-    @Query("SELECT c FROM certifications c INNER JOIN c.student_id std WHERE std.email = :email AND c.technology = :technology")
+    @Query("SELECT c FROM certifications c INNER JOIN c.studentEntity std WHERE std.email = :email AND c.technology = :technology")
     List<CertificationStudentEntity> findByStudentEmailAndTechnology(String email, String technology);
     
 }
